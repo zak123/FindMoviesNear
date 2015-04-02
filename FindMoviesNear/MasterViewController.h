f1cb4e4552eef2b2500cb69f6796a2497cdf2b6f
zak123 <zacharym123@gmail.com>
2015-04-02 10:56:32 -0700

    Initial Commit

---
+++ FindMoviesNear/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  FindMoviesNear
+//
+//  Created by Zachary Mallicoat on 4/2/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
